4e43cdec2ce60343c644545983ea61aab632f19b
Gezii <pigeon0312@gmail.com>
2014-12-24 17:06:48 +0800

    angular 依赖注入 双向数据绑定

--- web/js/angularjs/controllers.js
+++ web/js/angularjs/controllers.js
@@ -1,17 +1,31 @@
 /**
  * Created by Gezii on 2014/12/24.
  */
-function PhoneListCtrl($scope){
-    $scope.phones = [
+var PhoneListCtrl = ["$scope", "$http", function($scope, $http){
+    $scope.phones1 = [
         {"name" : "Xiaomi",
-         "snippet" : "Xiaomi 为发烧而生！"},
+         "snippet" : "为发烧而生！",
+         "age" : 3},
         {"name" : "Meizu",
-            "snippet" : "Meizu 喜欢就好！"},
-        {"name" : "Chuizi",
-            "snippet" : "Chuizi 东半球最好的手机！"},
+         "snippet" : "喜欢就好！",
+         "age" : 5},
+        {"name" : "Smartisan",
+         "snippet" : "东半球最好的手机！",
+         "age" : 2},
         {"name" : "Huwei",
-            "snippet" : "Huwei 华为！"}
+         "snippet" : "华为！",
+         "age" : 6},
+        {"name" : "Apple",
+         "snippet" : "呵呵！",
+         "age" : 6}
     ];
+    $scope.orderProp1 = "name";  //设置默认的排序为按name排序
 
     $scope.nums = [0,1,2,3,4,5,6,7];
-}
+
+
+    $http.get('../../data/phones/phones.json').success(function(data){
+        $scope.phones = data.splice(0, 5);
+    });
+    $scope.orderProp = "age";
+}];
