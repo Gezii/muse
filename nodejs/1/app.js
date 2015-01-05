c258008f0709aa4abdce37873ef9f1d76071a610
Gezii <pigeon0312@gmail.com>
2015-01-05 10:41:44 +0800

    update

--- nodejs/1/app.js
+++ nodejs/1/app.js
@@ -5,10 +5,13 @@
 //console.log("Hello World!");
 
 var http = require('http');
+var url = require('url');
+var util = require('util');
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World!!!!!!!!!123</p>');
+    res.write('<p>Hello World!!!!!!!!!123</p>');
+    res.end(util.inspect(url.parse(req.url, true)));
 }).listen(3000);
 console.log("HTTP server is listening at port 3000.");
