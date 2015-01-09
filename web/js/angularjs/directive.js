9fb5c0c167a19adb5a2c3297ca3079940e9f7ee0
Gezii <pigeon0312@gmail.com>
2015-01-09 16:00:45 +0800

    update 指令

--- web/js/angularjs/directive.js
+++ web/js/angularjs/directive.js
@@ -12,6 +12,7 @@ dirModule.run(function($templateCache){
 dirModule.directive('hello', function($templateCache){
     return {
         restrict: 'AEMC',   //A属性、E元素、M注释、C样式类
+        scope: {},          //独立指令，与其他的hello指令相互区别开
         templateUrl: $templateCache.get('hello'),
         replace: true
     };
@@ -23,4 +24,64 @@ dirModule.directive('trans', function(){
         transclude: true,
         template: '<div>Hello everyone!<div ng-transclude></div></div> '
     };
+});
+
+dirModule.controller('DirCtrl1', ['$scope', function($scope){
+    $scope.loadData1 = function(){
+        console.log("加载数据中1。。。");
+    };
+    $scope.ctrlFlavor = '百威';
+    $scope.sayHello = function(name){
+        alert('Hello ' + name);
+    }
+}]);
+
+dirModule.controller('DirCtrl2', ['$scope', function($scope){
+    $scope.loadData2 = function(){
+        console.log("加载数据中2。。。");
+    }
+}]);
+
+dirModule.directive('loader', function(){
+    return {
+        restrict: 'AE',
+        link: function(scope, element, attr){
+            element.bind('mouseenter',function(){
+//                scope.loadData();
+//                scope.$apply("loadData1()");
+                //howToLoad会被转换成小写的howtoload
+                scope.$apply(attr.howtoload);
+            })
+        }
+    }
+});
+
+
+dirModule.directive('drink', function(){
+    return {
+        restrict: 'AE',
+        scope: {
+//            flavor: '@'   //字符串传递
+            flavor: '='     //双向绑定
+        },
+        template: "<input type='text' ng-model='flavor' class='form-control'/>"
+//        ,
+//        link: function(scope, element, attrs){
+//            scope.flavor = attrs.flavor;
+//        }
+    }
+});
+
+dirModule.directive('greeting', function(){
+    return {
+        restrict: 'AE',
+        scope: {
+//            flavor: '@'
+//            flavor: '='     //双向绑定
+            greet: '&'       //传递一个来自父scope的函数，稍后调用
+        },
+        template: "<div class='col-md-9'><input type='text' ng-model='userName' class='form-control'/></div>"+
+                  "<div class='col-md-3'><button class='btn btn-default' ng-click='greet({name:userName})'>Greet</button></div>"
+
+    }
 });
